89316c7e85a202cc47bdd10df8d3a082bf5db624
Callum Anderson <cm20cja@leeds.ac.uk>
2024-07-11 20:57:39 +0100

    Hijacked atom logo to make my head roll

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Headshot-container">
+           <img src="icon.jpeg" className="Headshot" alt="cdog"/>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload hello changing things for a commit.
         </p>
