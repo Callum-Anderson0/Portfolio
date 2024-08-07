424bb07c5a12d54dc4b684b2672c1548e9625f28
Callum Anderson <cm20cja@leeds.ac.uk>
2024-08-07 13:44:24 +0100

    Scrollable right box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Headshot-container">
-           <img src="icon.jpeg" className="Headshot" alt="cdog"/>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload hello changing things for a commit.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Callum's Portfolio
       </header>
+      <div className="Main-Content">
+        <div className="Left-content"> left box
+          <div className="Headshot-container">
+            <img src="icon.jpeg" className="Headshot" alt="cdog"/>
+          </div>
+          <p>
+            Edit <code>src/App.js</code> and save to reload hello changing things for a commit.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+        <div className="Right-Content"> right box 
+          <div className="Scrollable-content">
+            <div className="Post" post1> post 1</div>
+            <div className="Post" post1> post 2</div>
+            <div className="Post" post1> post 3S</div>
+            <div className="Post" post1> post 1</div>
+            <div className="Post" post1> post 2</div>
+            <div className="Post" post1> post 3S</div>
+            <div className="Post" post1> post 1</div>
+            <div className="Post" post1> post 2</div>
+            <div className="Post" post1> post 3S</div>
+            <div className="Post" post1> post 1</div>
+            <div className="Post" post1> post 2</div>
+            <div className="Post" post1> post 3S</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
