360c9b880e85bb91bd8df121e0c733eef9dcb35c
Callum Anderson <cm20cja@leeds.ac.uk>
2024-08-07 14:58:49 +0100

    Making sure everything is comited

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
 import './App.css';
 
+
+
+
+
+function Post({title,image,content}){
+    return(
+      <div className="post">
+        <h2 className="post-title">{title}</h2>
+        {image && <img src={image} className="post-image"/>}
+        <p className="post-content">{content}</p>
+      </div>
+    );
+}
+
+
 function App() {
+  const posts = [
+    {
+      title: "First post",
+      image: "Post 1.jpg",
+      content: "In the first iteration of the portfolio I structured the html and the css to create an image for how I wanted the website to look. In terms of the general ballpark, entirely unstylised."
+    },
+
+    {
+      title: "Second Post",
+      image: "Post 2.jpg",
+      content: "In the next step I constructed this function? I think its called. Im not sure the technical react name but it seems to me it is a way to use javascript to build general html structures, in this case a post."
+    },
+    {
+      title: "3rd",
+      image: "",
+      content: ""
+    },
+    {
+      title: "4th",
+      image: "",
+      content: ""
+    },
+    {
+      title: "5th",
+      image: "",
+      content: ""
+    },
+  
+    
+    
+  ]
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,18 +73,14 @@ function App() {
         </div>
         <div className="Right-Content"> right box 
           <div className="Scrollable-content">
-            <div className="Post" post1> post 1</div>
-            <div className="Post" post1> post 2</div>
-            <div className="Post" post1> post 3S</div>
-            <div className="Post" post1> post 1</div>
-            <div className="Post" post1> post 2</div>
-            <div className="Post" post1> post 3S</div>
-            <div className="Post" post1> post 1</div>
-            <div className="Post" post1> post 2</div>
-            <div className="Post" post1> post 3S</div>
-            <div className="Post" post1> post 1</div>
-            <div className="Post" post1> post 2</div>
-            <div className="Post" post1> post 3S</div>
+            {posts.reverse().map((post, index)=> (
+              <Post
+                key = {index}
+                title = {post.title}
+                image = {post.image}
+                content = {post.content}
+              />
+            ))}
           </div>
         </div>
       </div>
